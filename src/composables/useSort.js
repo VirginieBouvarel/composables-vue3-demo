d3022929ea6bfb742d64276284f8caa1fdebde97
Virginie <vb@ikimo9.com>
2022-08-14 10:45:23 +0200

    PicturesGallery refacto avec composable

--- src/composables/useSort.js
+++ src/composables/useSort.js
@@ -2,7 +2,7 @@ import { ref } from 'vue';
 
 export default function useSort(items) {
   const currentItems = ref([]);
-console.log(items);
+
   function sortItemsBy(criteria) {
     const sortedItems = items.sort((a, b) => {
       if (a[criteria] < b[criteria]) return -1;
