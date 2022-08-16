61e3cd8862e2af08662f7b2a8c982e621b423614
Virginie <vb@ikimo9.com>
2022-08-16 09:23:27 +0200

    useSort ajout diffférentes fonctions tri

--- src/composables/useSort.js
+++ src/composables/useSort.js
@@ -1,20 +1,37 @@
-import { ref } from 'vue';
+import { ref, toRaw } from 'vue';
 
-export default function useSort(items) {
-  const currentItems = ref([]);
+export default function useSort(itemsToSort) {
+  const sortedItems = ref([]);
 
-  function sortItemsBy(criteria) {
-    const sortedItems = items.sort((a, b) => {
+  const sortByStrings = (criteria) => {
+    return itemsToSort.sort((a, b) => {
       if (a[criteria] < b[criteria]) return -1;
       if (a[criteria] > b[criteria]) return 1;
       return 0;
     });
-    currentItems.value = sortedItems;
+  }
+  const sortByNumbers = (criteria) => {
+    return itemsToSort.sort((a, b) => a[criteria] - b[criteria]);
+  }
+  const sortByDates = (criteria) => {
+    return itemsToSort.sort((a, b) => {
+      return new Date(a[criteria]) - new Date(b[criteria]);
+    });   
+  }
+  const sort = {
+    'string': sortByStrings,
+    'number': sortByNumbers,
+    'object': sortByDates,
+  }
+  function sortItemsBy(criteria) {
+    const items = toRaw(itemsToSort);
+    const type = typeof items[0][criteria];
+    sortedItems.value = sort[type](criteria);
   }
 
   return {
-    currentItems,
+    sortedItems,
     sortItemsBy,
   }
-  
+
 }
